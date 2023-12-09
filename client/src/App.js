720df74bf2d8f0b303f1454c9c9b6b6a6820e3d4
Pratyush Srivatava <pratyush0705@gmail.com>
2023-12-09 16:53:11 +0530

    feat: Change default page to dining

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import {Route, Routes, Navigate} from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom';
 import HomePage from './pages/Home.page';
 import RestrauntPage from './pages/Restraunt.page';
 import Checkout from './pages/Checkout.page';
@@ -11,22 +11,22 @@ import Order from './components/Restraunt/Order';
 import Menu from './components/Restraunt/Menu';
 import Review from './components/Restraunt/Review';
 import Photos from './components/Restraunt/Photos';
-import {useDispatch} from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { getMySelf } from './redux/reducers/user/user.action';
 
 function App() {
   const dispatch = useDispatch();
 
-  useEffect(() =>{
+  useEffect(() => {
     dispatch(getMySelf());
-  },[localStorage])
-  
+  }, [localStorage]);
+
   return (
     <Routes>
-      <Route path='/' element={<Navigate to="/delivery" />}></Route>
+      <Route path='/' element={<Navigate to='/dining' />}></Route>
       <Route path='/:type' element={<HomePage />}></Route>
       <Route path='/restraunt/:id' element={<RestrauntPage />}>
-        <Route path ='overview' element={<Overview />}></Route>
+        <Route path='overview' element={<Overview />}></Route>
         <Route path='order' element={<Order />}></Route>
         <Route path='menu' element={<Menu />}></Route>
         <Route path='photos' element={<Photos />}></Route>
