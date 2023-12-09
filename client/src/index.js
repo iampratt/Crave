e31088e8e0ca5f9c1a2f2cad28f2bea09dc3f2de
Pratyush Srivatava <pratyush0705@gmail.com>
2023-12-09 19:11:36 +0530

    fix: vercel insights

--- client/src/index.js
+++ client/src/index.js
@@ -6,19 +6,15 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import axios from 'axios';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 if (localStorage.LoggedInUser) {
   const { token } = JSON.parse(localStorage.LoggedInUser);
   axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
 root.render(
-  <>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-    <SpeedInsights />
-  </>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
