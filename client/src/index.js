9622b583cc888ae017bbaa47c330897d6a3f646e
Pratyush Srivatava <pratyush0705@gmail.com>
2023-12-09 17:47:07 +0530

    feat: added title

--- client/src/index.js
+++ client/src/index.js
@@ -3,20 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux';
 import store from './redux/store';
-import axios from 'axios'
+import axios from 'axios';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 if (localStorage.LoggedInUser) {
   const { token } = JSON.parse(localStorage.LoggedInUser);
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
+document.title = 'CRAVE';
 root.render(
-  <Provider store={store} >
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>
 );
-
-
